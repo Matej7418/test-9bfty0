1e16c133324097d071f6b30188fc12423f4815f0
SoulHPP <kristinakucerka20@gmail.com>
2023-03-07 13:15:28 +0100

    update

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,13 +11,30 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
+      
       <ExploreContainer name="Tab 1 page" />
+      <ion-button shape="round">Button</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts">
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+  import ExploreContainer from '@/components/ExploreContainer.vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: { ExploreContainer, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton },
+  });
 </script>
+
+<style scoped>
+  body {
+    text-align: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+</style>
